82a6e1e785588638f69bb6a1607669866616f7d0
ThomasLafeber <145574730+ThomasLafeber@users.noreply.github.com>
2023-09-21 13:10:02 +0200

    de customizer
    de customizer erbij

---
+++ custom-pizza.php
@@ -0,0 +1,33 @@
+<html>
+<body>
+
+<h1>Wat wilt u op uw pizza?
+U kunt ook onze <li><a href="index.php">vaste pizza's</a></li>nemen</h1>
+<h2>
+<form>
+ <form action="verwerk.php" method="POST">
+  Tomatensaus<input type="checkbox" id="tomatensaus" name="tomatensaus"><br>
+  Ananas<input type="checkbox" id="ananas" name="ananas"><br>
+  Salami<input type="checkbox" id="salami" name="salami"><br>
+  Kaas<input type="checkbox" id="kaas" name="kaas"><br>
+  Knoflooksaus<input type="checkbox" id="knoflooksaus" name="knoflooksaus"><br>
+  Champignons<input type="checkbox" id="champignons" name="champignons"><br><br>
+  <input type="submit"  id="submit" name="verstuur"><br>
+  
+</form>
+Wilt u een 2e pizza? Zo ja, wat wilt u er op?
+<form>
+ <form action="verwerk.php" method="POST">
+ 2e pizza? <input type="checkbox" id="pizza2" name="pizza2"><br>
+  Tomatensaus<input type="checkbox" id="tomatensaus2" name="tomatensaus2"><br>
+  Ananas<input type="checkbox" id="ananas2" name="ananas2"><br>
+  Salami<input type="checkbox" id="salami2" name="salami2"><br>
+  Kaas<input type="checkbox" id="kaas2" name="kaas2"><br>
+  Knoflooksaus<input type="checkbox" id="knoflooksaus2" name="knoflooksaus2"><br>
+  Champignons<input type="checkbox" id="champignons2" name="champignons2"><br><br>
+  <input type="submit"  id="submit" name="verstuur"><br>
+ 
+</form>
+</h2>
+</body>
+</html>
