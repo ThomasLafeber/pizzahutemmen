2e12edc8666e21bcea55c07b15717b4d2c539a90
ThomasLafeber <145574730+ThomasLafeber@users.noreply.github.com>
2023-09-22 12:24:14 +0200

    hele index

---
+++ index.php
@@ -0,0 +1,97 @@
+<html lang="nl">
+
+ 
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Pizza Hut Emmen</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap" rel="stylesheet">
+
+    <link href="style.css" rel="stylesheet">
+
+    <link href="normalize.css" rel="stylesheet">
+
+ 
+
+</head>
+
+ 
+
+<body>
+
+ 
+
+    <navbar>
+
+        <div class="logo">
+
+            <a href="index.php"><img src="img/logo.png" alt="logo"></a>
+
+        </div>
+
+        <ul>
+
+            <li><a href="index.php" class="active">Home</a></li>
+
+            <li><a href="contact.html">Contact</a></li>
+
+            <li><a href="custom-pizza.php" class="btn secondary small">Maak je eigen pizza <span>❯</span></a></li>
+
+        </ul>
+
+    </navbar>
+
+    <header>
+
+        <div class="intro-text">
+
+            <div class="inner">
+
+                <h1>Welkom bij pizzahut <span>Emmen</span></h1>
+
+                <p>De beste pizza's van Nederland</p>
+
+                <div style="display: flex;">
+
+                    <a href="#menu" class="btn primary">Bekijk ons menu <span>❯</span></a>
+
+                    <a href="custom-pizza.php" class="btn secondary" style="margin-left: 1em;">Maak je eigen pizza
+
+                        <span>❯</span></a>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="header-image">
+
+            <div class="inner">
+
+                <img src="img/header-img.png" alt="rot op">
+
+                <div class="shape"></div>
+
+            </div>
+
+        </div>
+
+    </header>
+
+ 
+
+</body>
+
+ 
+
+</html>
