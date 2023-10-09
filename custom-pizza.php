1c00b7d6637edfdf951d82a0675e33aa9624312c
ThomasLafeber <145574730+ThomasLafeber@users.noreply.github.com>
2023-10-09 13:31:26 +0200

    form weer goed

--- custom-pizza.php
+++ custom-pizza.php
@@ -22,6 +22,7 @@
 <h1>
 <h2>
 Wat wilt u op uw pizza? <br>
+<form action="custom-pizza.php" method="POST">
   Tomatensaus<input type="checkbox" id="tomatensaus" name="tomatensaus" value="tomatensaus" class="knopje"><br>
   Ananas<input type="checkbox" id="ananas" name="ananas" value="ananas, " ><br>
   Salami<input type="checkbox" id="salami" name="salami" value="salami, "><br>
