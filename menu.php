448cae1a2a849a58a4bb034c7bd3c99edd177012
ThomasLafeber <145574730+ThomasLafeber@users.noreply.github.com>
2023-10-16 15:40:03 +0200

    het nieuwe menu

---
+++ menu.php
@@ -0,0 +1,24 @@
+<html>
+
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+ <navbar>
+        <div class="logo">
+            <a href="index.php"><img src="img/logo.png" alt="logo"></a>
+        </div>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+                <li><a href="menu.php"  class="active">Menu</a></li>
+            <li><a href="contact.html">Contact</a></li>
+            <li><a href="custom-pizza.php" class="btn secondary small">Maak je eigen pizza <span>❯</span></a></li>
+        </ul>
+    </navbar>
+    </body>
+
+    </html>
