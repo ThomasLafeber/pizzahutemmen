4cc027efed768370ac9124805332483223a8259d
mees <145574578+Meesdonja@users.noreply.github.com>
2023-11-08 15:29:11 +0100

    oude code terug

--- custom-pizza.php
+++ custom-pizza.php
@@ -1,12 +1,10 @@
 <html>
-
 <head>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap" rel="stylesheet">
     <link href="style.css" rel="stylesheet">
 </head>
-
 <body>
  <navbar>
         <div class="logo">
@@ -26,54 +24,49 @@
 Wat wilt u op uw pizza? <br>
 <form action="custom-pizza.php" method="POST">
 
-  Tomatensaus<input type="checkbox" id="tomatensaus" name="tomatensaus" value="tomatensaus, " class="knopje"><br>
-   Kaas<input type="checkbox" id="kaas" name="kaas" value="kaas, " class="knopje"><br>
- Salami<input type="checkbox" id="salami" name="salami" value="salami, " class="knopje" onclick="wegVis()"><br>
-  Vis<input type="checkbox" id="vis" name="vis" value="vis, " class="knopje" onclick="wegSal()"><br>
+  Tomatensaus <input type="checkbox" id="tomatensaus" name="tomatensaus" value="tomatensaus" class="knopje"><br>
+  Tomatensaus <input type="checkbox" id="tomatensaus" name="tomatensaus" value="tomatensaus, " class="knopje"><br>
+   Kaas<input type="checkbox" id="kaas" name="kaas" value="kaas, " class="knopje" onclick="addKaas()"><br>
+ Salami<input type="checkbox" id="salami" name="salami" value="salami, " class="knopje" onclick="addSalami()"><br>
+  Vis<input type="checkbox" id="vis" name="vis" value="vis, " class="knopje" onclick="addVis()"><br>
    Champignons<input type="checkbox" id="champignons" name="champignons" value=" champignons, " class="knopje"><br>
-  Ananas<input type="checkbox" id="ananas" name="ananas" value="ananas, " class="knopje" onclick="anasbij()"><br>
+  Ananas<input type="checkbox" id="ananas" name="ananas" value="ananas, " class="knopje" onclick="addAnanas()"><br>
   Knoflooksaus<input type="checkbox" id="knoflooksaus" name="knoflooksaus" value="knoflooksaus, " class="knopje"><br><br>
   
   <input type="submit"  id="submit" name="verstuur"><br>
 </form>
 </div>
-
 <?php
 if($_SERVER["REQUEST_METHOD"] == "POST")
 {
-  $tomatensaus = $_POST["tomatensaus"];
-  $ananas = $_POST["ananas"];
-  $kaas = $_POST["kaas"];
-  $salami = $_POST["salami"];
-  $champignons = $_POST["champignons"];
-  $knoflooksaus = $_POST["knoflooksaus"];
-
-  $pizza2 = $_POST["pizza2"];
-  $tomatensaus2 = $_POST["tomatensaus2"];
-  $ananas2 = $_POST["ananas2"];
-  $kaas2 = $_POST["kaas2"];
-  $salami2 = $_POST["salami2"];
-  $champignons2 = $_POST["champignons2"];
-  $knoflooksaus2 = $_POST["knoflooksaus2"];
-
+  if(isset($_POST["tomatensaus"]))
+    $tomatensaus = $_POST["tomatensaus"];
+  if(isset($_POST["ananas"]))
+    $ananas = $_POST["ananas"];
+  if(isset($_POST["kaas"]))
+    $kaas = $_POST["kaas"];
+  if(isset($_POST["salami"]))
+    $salami = $_POST["salami"];
+  if(isset($_POST["champignons"]))
+    $champignons = $_POST["champignons"];
+  if(isset($_POST["knoflooksaus"]))
+    $knoflooksaus = $_POST["knoflooksaus"];
   echo "Uw bestelling is een pizza met: <br><br>";
   echo $tomatensaus . $kaas . $salami . $champignons . $ananas . $knoflooksaus;
 }
 ?>
 <br>
 <br>
-
  </h2>
 </h1>
 <div class="lege-pizza">
-
             <div class="inner">
-
                 <img src= "img/lege-pizza.png" alt="PIZZA JUMPSCARE">
                 <img id="ananass" src="img/ananass.jpg" style="display:none;">
-
+                <img id="salamie" src="img/salami.png" style="display:none;">
+                <img id="viss" src="img/vis.png" style="display:none;">
+                <img id="kaass" src="img/kaass.jpg" style="display:none;">
             
-
                       
             </div> 
         </div>
@@ -81,40 +74,76 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
   <footer>
  ‎<div id="main">
 <div> </div>
-<div> <br> <b> Creator of the website </b> <br><br> <a style="text-decoration: none;" href="mailto:133582@hondsrugcollege.nl">Jonah van der Lugt</a> <br> <a style="text-decoration: none;" href="mailto:133271@hondsrugcollege.nl">Thomas Lafeber</a> <br> <a style="text-decoration: none;" href="mailto:133041@hondsrugcollege.nl">Mees Herr</a> <br><br></div>
+<div> <br> <b> Creators of the website </b> <br><br> <a style="text-decoration: none;" href="mailto:133582@hondsrugcollege.nl">Jonah van der Lugt</a> <br> <a style="text-decoration: none;" href="mailto:133271@hondsrugcollege.nl">Thomas Lafeber</a> <br> <a style="text-decoration: none;" href="mailto:133041@hondsrugcollege.nl">Mees Herr</a> <br><br></div>
 <div style="text-decoration: none;"> <br><b>Owner PH-emmen</b>  <br><br> <a style="text-decoration: none;" href="mailto:SmiR@hondsrugcollege.nl"> Rob </a> </div>
 <div style="text-decoration: none;"> <br><b>Locatie</b> <br><br> <a> Emmalaan 25 </a> </div>
 <div style="text-decoration: none;"> <br><b>Socials</b> <br><br> <a style="text-decoration: none;" href="https://www.pizzahut.be/nl">Officiële Pizza Hut </a></div> 
 <div> </div>
 </div>
 </footer>
-
 </body>
 <script>
-
-function wegVis(){
-  if(document.getElementById("vis").style.display == "none"){
+function addSalami()
+{
+  //Knopje vis weg
+  if(document.getElementById("vis").style.display == "none")
+  {
     document.getElementById("vis").style.display = "inline";
-  }else{
-  document.getElementById("vis").style.display = "none";
+  }
+  else
+  {
+    document.getElementById("vis").style.display = "none";
+  }
+  //Salami plaatje toevoegen
+  if(document.getElementById("salamie").style.display == "none"){
+      document.getElementById("salamie").style.display = "inline";
+    }else{
+    document.getElementById("salamie").style.display = "none";
+  }
 }
-}
-
-function wegSal(){
-  if(document.getElementById("salami").style.display == "none"){
+function addVis()
+{
+  //Knopje salami weg
+  if(document.getElementById("salami").style.display == "none")
+  {
     document.getElementById("salami").style.display = "inline";
-  }else{
+  }else
+  {
   document.getElementById("salami").style.display = "none";
+  }
+ //Vis plaatje toevoegen
+  if(document.getElementById("viss").style.display == "none")
+  {
+      document.getElementById("viss").style.display = "inline";
+  }else
+  {
+    document.getElementById("viss").style.display = "none";
+  }
 }
-}
-
-function anasbij(){
-  if(document.getElementById("ananass").style.display == "none"){
+function addAnanas()
+{
+  //Plaatje ananas toevoegen
+   if(document.getElementById("ananass").style.display == "none")
+   {
     document.getElementById("ananass").style.display = "inline";
-  }else{
-  document.getElementById("ananass").style.display = "none";
+   }else
+   {
+    document.getElementById("ananass").style.display = "none";
+   }
 }
+function addKaas()
+{
+  //Plaatje kaas toevoegen
+   if(document.getElementById("kaass").style.display == "none")
+  {
+    document.getElementById("kaass").style.display = "inline";
+  }else
+  {
+    document.getElementById("kaass").style.display = "none";
+  }
 }
-
+function salamiBijPlaatje(){
+  
+}
 </script>
 </html>
